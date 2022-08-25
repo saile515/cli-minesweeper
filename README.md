2ae09e1ca102fe9c3e65f0ed94659dcd4d7c8342
saile515 <elias.jorgensen2006@gmail.com>
2022-08-25 19:19:14 +0200

    Correct beginning explosion

--- README.md
+++ README.md
@@ -4,7 +4,13 @@
 
 ```console
     npm i
-    npm start
+    npm start (width?) (height?) (mines?)
 ```
+
+When playing the game, first enter the x coordinate, then the y coordinate, and finally the command.
+
+### Commands
+* pop (pops the cell to reveal what is underneath)
+* flag (marks that the cell contains a mine)
 
 *The PseudoCode folder is only included due to this being a school assignment.*
--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,8 @@ export class Game {
 	width: number;
 	height: number;
 	mines: number;
-	lost: boolean;
+	minesPlaced: boolean;
+	completed: boolean;
 
 	grid: GridCell[];
 	screen: Screen;
@@ -18,7 +19,8 @@ export class Game {
 		this.height = height;
 		this.grid = [];
 		this.mines = mines;
-		this.lost = false;
+		this.minesPlaced = false;
+		this.completed = false;
 
 		// Instantiate cells
 		for (let y = 1; y <= this.height; y++) {
@@ -27,36 +29,12 @@ export class Game {
 			}
 		}
 
-		// Place mines
-		for (let i = 0; i < this.mines; i++) {
-			const nonMineCells = this.grid.filter((cell) => !cell.mine);
-			const random = Math.floor(Math.random() * nonMineCells.length);
-			nonMineCells[random].mine = true;
-		}
-
-		// Calculate cell values
-		this.grid.forEach((cell) => {
-			let count = 0;
-			for (let x = cell.x > 1 ? -1 : 0; x <= (cell.x < this.width ? 1 : 0); x++) {
-				for (let y = cell.y > 1 ? -1 : 0; y <= (cell.y < this.height ? 1 : 0); y++) {
-					if (x == 0 && y == 0) continue;
-
-					const neighborCell = this.grid.find((item) => item.x == x + cell.x && item.y == y + cell.y);
-
-					if (neighborCell?.mine) count++;
-				}
-			}
-			cell.value = count;
-		});
-
-		console.log(width * height, this.grid.length);
-
 		this.screen = new Screen(this);
 
 		let emptyCells = this.calculateEmptyCells();
 
-		while (emptyCells != 0 && !this.lost) {
-			const input: string[] = prompt()("Input: ").toString().split(" ");
+		while (emptyCells != 0 && !this.completed) {
+			const input: string[] = prompt()("Input (x, y, cmd): ").toString().split(" ");
 
 			const x = parseInt(input[0]);
 			const y = parseInt(input[1]);
@@ -69,28 +47,43 @@ export class Game {
 
 			this[func!](x, y);
 			emptyCells = this.calculateEmptyCells();
-			if (!this.lost) this.screen.update();
+			if (emptyCells == 0) {
+				console.log("You win!");
+				this.completed = true;
+			}
+			if (!this.completed) this.screen.update();
 		}
 	}
 
 	clickCell(x: number, y: number) {
-		const cell = this.grid.find((cell) => cell.x == x && cell.y == y)!;
+		const cell = this.getCell(x, y);
+
+		if (!this.minesPlaced) {
+			const neighbors = this.getNeighbors(cell, true);
+			this.placeMines(neighbors);
+			this.minesPlaced = true;
+		}
+
 		if (!cell) {
 			console.log("No cell with those coordinates exist! Try again.");
 			return;
 		}
 
 		if (cell.state != CellState.Hidden) {
-			console.log("Cell already popped! Try again.");
+			console.log("Cell can't be popped! Try again.");
 			return;
 		}
 
 		if (cell.mine) {
 			console.log("You lose!");
-			this.lost = true;
+			this.completed = true;
 			return;
 		} else {
 			cell.state = CellState.Visible;
+		}
+
+		if (cell.value == 0) {
+			this.popNeighbors(cell);
 		}
 	}
 
@@ -103,9 +96,65 @@ export class Game {
 
 		if (cell.state == CellState.Flagged) {
 			cell.state = CellState.Hidden;
-		} else {
+		} else if (cell.state == CellState.Hidden) {
 			cell.state = CellState.Flagged;
+		} else {
+			console.log("Cell can't be flagged, as it is already visible!");
 		}
+	}
+
+	getCell(x: number, y: number) {
+		return this.grid.find((cell) => cell.x == x && cell.y == y)!;
+	}
+
+	getNeighbors(cell: GridCell, includeSelf?: boolean) {
+		const x = cell.x;
+		const y = cell.y;
+
+		const cells: GridCell[] = [];
+		for (let xCoord = x > 1 ? -1 : 0; xCoord <= (x < this.width ? 1 : 0); xCoord++) {
+			for (let yCoord = y > 1 ? -1 : 0; yCoord <= (y < this.height ? 1 : 0); yCoord++) {
+				if (x == xCoord && y == yCoord && !includeSelf) continue;
+				const neighborCell = this.grid.find((item) => item.x == x + xCoord && item.y == y + yCoord)!;
+
+				cells.push(neighborCell);
+			}
+		}
+
+		return cells;
+	}
+
+	popNeighbors(cell: GridCell) {
+		const neighbors = this.getNeighbors(cell);
+
+		neighbors.forEach((item) => {
+			if (item.state != CellState.Hidden) return;
+			item.state = CellState.Visible;
+			if (item.value == 0 && item.state) this.popNeighbors(item);
+		});
+	}
+
+	placeMines(avoid?: GridCell[]) {
+		const nonMineCells = this.grid.filter((cell) => !cell.mine);
+		if (avoid)
+			for (let i = 0; i < avoid.length; i++) {
+				const index = nonMineCells.indexOf(avoid[i]);
+				nonMineCells.splice(index, 1);
+			}
+
+		for (let i = 0; i < this.mines; i++) {
+			const random = Math.floor(Math.random() * nonMineCells.length);
+			nonMineCells[random].mine = true;
+			nonMineCells.splice(random, 1);
+		}
+
+		// Calculate cell values
+		this.grid.forEach((cell) => {
+			const neighbors = this.getNeighbors(cell);
+			let count = 0;
+			neighbors.forEach((item) => (item.mine ? count++ : null));
+			cell.value = count;
+		});
 	}
 
 	calculateEmptyCells() {
@@ -114,4 +163,13 @@ export class Game {
 	}
 }
 
-const game = new Game(10, 10, 30);
+const width = parseInt(process.argv[2]) || 10;
+const height = parseInt(process.argv[3]) || 10;
+const mines = parseInt(process.argv[4]) || (width * height) / 4;
+
+if (mines >= width * height) {
+	console.log("Mines must be lower than grid size!");
+	process.abort();
+}
+
+const game = new Game(width, height, mines);
